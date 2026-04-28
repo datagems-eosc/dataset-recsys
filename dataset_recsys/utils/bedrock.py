57d2f553e8aecd577e2a77b2db8b6f6179270307
Kostas Mparmparousis <mparmparousis.kostas@gmail.com>
2026-04-28 18:22:47 +0300

    Env example update

--- dataset_recsys/utils/bedrock.py
+++ dataset_recsys/utils/bedrock.py
@@ -1,11 +1,11 @@
 import boto3
 import json
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 from dataset_recsys.ingestion.fetch_gems_datasets import DatasetProfile
 
-load_dotenv()
+# load_dotenv()
 
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
